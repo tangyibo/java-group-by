c0cc72f2edf01b7f42d7d9b94f0662d76bc22f96
tang <tang@126.com>
2021-02-21 19:11:02 +0800

    参数调整测试

--- run.cmd
+++ run.cmd
@@ -8,7 +8,7 @@ setlocal enabledelayedexpansion
 cls
 
 ::java虚拟机启动参数
-set MAVEN_OPTS=-server -Xms6g -Xmx6g -Xmn512m -XX:+DisableExplicitGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Djava.awt.headless=true -Dfile.encoding=UTF-8
+set MAVEN_OPTS=-server -Xms4g -Xmx4g -Xmn512m -XX:+DisableExplicitGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Djava.awt.headless=true -Dfile.encoding=UTF-8
 
 echo "Clean Project ..."
 call mvn clean -f pom.xml
