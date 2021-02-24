b846937ccde983314ff928fcda8316c6a241bccb
Yibo <inrgihc@126.com>
2021-02-24 11:36:36 +0800

    更新MinimumMemoryMap的entrySet()

--- java-group-by-tang/src/main/java/com/github/tang/groupby/core/MinimumMemoryMap.java
+++ java-group-by-tang/src/main/java/com/github/tang/groupby/core/MinimumMemoryMap.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -184,7 +185,7 @@ public class MinimumMemoryMap<K, V> implements Map<K, V> {
 	@Override
 	public Set<Map.Entry<K, V>> entrySet() {
 		int size = keys.size();
-		Set<Map.Entry<K, V>> entrySet = new HashSet<Map.Entry<K, V>>(size);
+		Set<Map.Entry<K, V>> entrySet = new LinkedHashSet<Map.Entry<K, V>>(size);
 
 		for (int index = 0; index < size; index++) {
 			entrySet.add(new AbstractMap.SimpleEntry<K, V>(keys.get(index), values.get(index)));
